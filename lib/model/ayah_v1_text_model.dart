b34e56686fd9ac69fdb879be1d7dbd1ec84cdcc1
tofi1130 <taafuuu@gmail.com>
2024-04-22 23:50:09 +0500

    add v1 code ayah text model

---
+++ lib/model/ayah_v1_text_model.dart
@@ -0,0 +1,11 @@
+class AyahV1 {
+  int pageNumber;
+  String verseKey;
+  String verseText;
+
+  AyahV1({
+    required this.pageNumber,
+    required this.verseKey,
+    required this.verseText,
+  });
+}
